7be2cdb71abd3e55c22cc76a7267d83328f970a7
Jacobo Cardona <jaclob7@gmail.com>
2024-06-24 22:11:01 -0500

    Primera version de la tarea pytest

---
+++ Pruebas.py
@@ -0,0 +1,12 @@
+def suma(x,y):
+    return x+y
+def resta(x,y):
+    return x-y
+def multiplicacion(x,y):
+    return x*y
+def division(x,y):
+    if y == 0:
+        print("Division por cero")
+        return None
+    else:
+        return x/y
---
+++ test_Pruebas.py
@@ -0,0 +1,40 @@
+import pytest 
+from Pruebas import suma,resta,multiplicacion,division
+
+def test_suma():
+    assert suma(-2,-3) == -5
+    print("La función suma trabaja correctamente")
+
+def test_suma2():
+    assert suma(51,6) == 57
+    print("La función suma trabaja correctamente")
+    
+def test_resta():
+    assert resta(2,-3) == 5
+    print ("La funcion resta trabaja correctamente")
+    
+def test_resta2():
+    assert resta (-2,2) == -4
+    print ("La funcion resta trabaja correctamente")
+
+def test_multiplicacion():
+    assert multiplicacion (2,-3) == -6
+    print ("La función multiplicacion trabaja correctamente")
+
+def test_multiplicacion():
+    assert multiplicacion (-5,-5) == 25
+    print ("La función multiplicacion trabaja correctamente")
+    
+def test_division():
+    assert division (2,0) == None
+    print ("La función division trabaja correctamente")
+    
+def test_division2():
+    assert division (-4,2) == -2
+    print ("La función division trabaja correctamente")
+    
+def test_division3():
+    assert division (-8,-4) == 2
+    print ("La función division trabaja correctamente")
+    
+    
